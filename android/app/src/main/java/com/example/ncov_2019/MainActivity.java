f02bfbecbc80e2604c154f392adb74cf8b4cd75b
ahyangnb <zonggeu@gmail.com>
2020-01-26 03:17:38 +0800

    commit init

---
+++ android/app/src/main/java/com/example/ncov_2019/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ncov_2019;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
