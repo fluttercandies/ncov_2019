e66c92e51a89b60a186124d89aaa5b10c62db3f7
ahyangnb <zonggeu@gmail.com>
2020-01-27 21:40:52 +0800

    commit pop

--- lib/widget/view/web_view_page.dart
+++ lib/widget/view/web_view_page.dart
@@ -16,37 +16,55 @@ class WebViewPage extends StatefulWidget {
 
 class WebViewPageState extends State<WebViewPage> {
   final Completer<WebViewController> _controller =
-  new Completer<WebViewController>();
+      new Completer<WebViewController>();
+  WebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: new ComMomBar(title: widget.title ?? ''),
-      body: new Builder(builder: (BuildContext context) {
-        return new WebView(
-          initialUrl: widget.url,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          javascriptChannels: <JavascriptChannel>[
-            _toasterJavascriptChannel(context),
-          ].toSet(),
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.startsWith('https://github.com/flutterbear')) {
-              print('blocking navigation to $request}');
-              return NavigationDecision.prevent;
-            }
-            print('allowing navigation to $request');
-            return NavigationDecision.navigate;
-          },
-          onPageFinished: (String url) {
-            print('Page finished loading: $url');
-          },
-        );
-      }),
+      body: new WillPopScope(
+        child: new Builder(builder: (BuildContext context) {
+          return new WebView(
+            initialUrl: widget.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+              _webViewController = webViewController;
+            },
+            javascriptChannels: <JavascriptChannel>[
+              _toasterJavascriptChannel(context),
+            ].toSet(),
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url
+                  .startsWith('https://github.com/ahyangnb/ncov_2019')) {
+                print('blocking navigation to $request}');
+                return NavigationDecision.prevent;
+              }
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+            },
+          );
+        }),
+        onWillPop: () => pop(),
+      ),
     );
+  }
+
+  Future<bool> pop() async {
+    bool canGoBack = await _webViewController.canGoBack();
+
+    if (canGoBack) {
+      _webViewController.goBack();
+
+      return false;
+    } else {
+      return true;
+    }
   }
 
   JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
