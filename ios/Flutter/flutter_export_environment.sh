ced6ba775426956e752ca6adbc41314014d14009
ahyangnb <zonggeu@gmail.com>
2020-10-20 11:04:17 +0800

    commit 09.03

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/q1/flutter"
+export "FLUTTER_ROOT=/Users/q1/flutterNew/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/q1/Documents/gitPro/ahyangnb/ncov_2019"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/q1/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/q1/flutterNew/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.10"
 export "FLUTTER_BUILD_NUMBER=10"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
